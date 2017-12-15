05f91daefcf3572c74fcf975c024964b4a3a70d8
Olivier van der Sijs <oliviervandersijs@gmail.com>
2017-12-15 15:54:54 +0100

    to lower case

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -179,7 +179,7 @@ public class Controller {
         controlAddButton.setOnAction(event -> {
             String name = controlAddNaam.getText();
 
-            if(!TrainFacade.getInstance().validTrainName(name)){
+            if(!TrainFacade.getInstance().validTrainName(name.toLowerCase())){
                 writeToConsole("Voer een geldige naam in.");
             } else {
 
